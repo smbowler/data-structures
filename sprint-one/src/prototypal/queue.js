b8c5081c3245861962ce97341c560f4e2e769caa
Samuel Bowler <samuel.bowler@gmail.com>
2015-10-09 14:08:16 -0500

    Passed all tests for Prototypal Queue and Stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,53 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someQueue = Object.create(queueMethods);
+  someQueue.front = 0;
+  someQueue.back = 0;
+  someQueue.storage = {};
+
+
+  return someQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  
+  enqueue : function(value){
+   this. back++;
+   this.storage[this.back] = value;
+  },
+  
+  dequeue : function(){
+    if (this.size() > 0){
+      this.front++;
+      var result = this.storage[this.front];
+      delete this.storage[this.front];
+    }
+    return result;
+  },
 
+  size : function(){
+    return this.back - this.front;
+  }
+
+};
+
+//
+// Queue.prototype.enqueue = function(value){
+//   this.back++;
+//   this.storage[this.back] = value;
+// };
+
+// Queue.prototype.dequeue = function(){
+//   if (this.size() > 0){
+//     this.front++;
+//     var result = this.storage[this.front];
+//     delete this.storage[this.front];
+//   }
+//   return result;
+// };
+
+// Queue.prototype.size = function(){
+//   return this.back - this.front;
+// };
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,66 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someStack = Object.create(stackMethods);
+  someStack.currentSize = 0;
+  someStack.storage = {};
+
+  return someStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function (value) {
+    this.currentSize++;
+    this.storage[this.currentSize] = value;
+  },
+
+  pop : function () {
+    if (this.currentSize > 0) {
+      // is result a copy of this.storage[this.currenSize]?
+      // if it referred to the same thing, then returning result
+      // would be undefined, right?
+      /*
+        [1, 2, 3]
+        'hello'
+        var result = this.storage[this.currentSize].slice();
+      */
+      var result = this.storage[this.currentSize];
+      delete this.storage[this.currentSize];
+      this.currentSize--;
+    }
+    return result;
+  },
+
+  size : function () {
+    return this.currentSize;
+  }
+
+};
 
 
+/*
+Stack.prototype.push = function (value) {
+  this.currentSize++;
+  this.storage[this.currentSize] = value;
+};
+*/
+
+/*
+Stack.prototype.pop = function () {
+  if (this.currentSize > 0) {
+    // is result a copy of this.storage[this.currenSize]?
+    // if it referred to the same thing, then returning result
+    // would be undefined, right?
+
+    var result = this.storage[this.currentSize];
+    delete this.storage[this.currentSize];
+    this.currentSize--;
+  }
+  return result;
+};
+
+Stack.prototype.size = function () {
+  return this.currentSize;
+};
+*/
