3cff892f1dbfd997ede8bacf80a2504ef12365ee
Samuel Bowler <samuel.bowler@gmail.com>
2015-10-09 21:54:25 -0500

    Sprint 2 - Passed all tests for tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,66 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
+
+
+  /*
+  tree_instance = {
+    value : value,
+    children : [
+                  {value: val, children: []}, 
+                  {value: val, children: []}, 
+                  {value: val, children: []}
+                                                ]
+  }
+  */
+
+
 };
-
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree();
+  child.value = value;
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
+  var result = false;
 
+  var recurse = function (node) {
+    //check base case: is length of childrenarray == 0?
+    if (node.value === target) {
+      result = true;
+    }
+    if (node.children.length) {
+      for (var i = 0; i < node.children.length; i++){
+        recurse(node.children[i]);
+      }
+    }
+
+    /*
+    if (node.children.length === 0) {
+      return;
+    }
+    for (var i=0; i<node.children.length; i++) {
+      recurse(node.children[i]);
+    }
+    */    
+  };
+
+  console.log(this);
+  recurse(this);
+
+  return result;
 };
 
 
